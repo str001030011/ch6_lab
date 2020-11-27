429cfe5fbeed249738eb056c7e08d380d88fa4a1
str001030011 <str001030011@gmail.com>
2020-11-27 20:46:57 +0800

    first

---
+++ CH6_Lab1/include/Information.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<stdio.h>
+typedef struct _sPerson {
+
+	char name[8];
+	int gender;
+	int age;
+}Person;
---
+++ ch6_lab3/include/Info.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+typedef struct _student {
+
+	char num[7];	//學號
+	char clas[20]; //班級
+	char name[20];  //姓名
+	int score[3];	//各科成績 score[0]國文 score[1]英文 score[2]數學
+
+}student;
